686cba4666289bd961a0f6846e3b60c10f03f74d
UALERSON <mediacutsstudio@gmail.com>
2025-05-07 22:09:12 -0300

    Add Integration.py

---
+++ AgentsWorkFlow/Saas/Code/DevOps/DockerFile/Integration.py
@@ -0,0 +1,90 @@
+from agents import Agent, handoff, RunContextWrapper
+import requests
+from dotenv import load_dotenv, find_dotenv
+import os
+from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX
+from pydantic import BaseModel
+
+from modules.modules import *
+from modules.Egetoolsv2 import *
+from modules.EgetMetadataAgent import *
+
+
+from AgentsWorkFlow.Saas.Code.DevOps.DockerCompose.Integration import CodeDockerComposeAgent
+
+class FrontEndData(BaseModel):
+    FrontEndContent: str
+
+async def on_handoff(ctx: RunContextWrapper[None], input_data: FrontEndData):
+    print(f"CodeDockerFileAgent: {input_data.FrontEndContent}")
+    # response = requests.post("http://localhost:5000/agent/refund", json={"input": input_data.reason})
+    # reply = response.json().get("reply")
+              
+def CodeDockerFileAgent(session_id, appcompany,
+                        path_ProjectWeb,
+                        path_html,
+                        path_js,
+                        path_css,
+                        doc_md,
+                        Keys_path,
+                    ):
+   
+    os.chdir(path_ProjectWeb)
+
+
+    agent_, handoff_obj_CodeDockerComposeAgent = CodeDockerComposeAgent(session_id, appcompany,
+                        path_ProjectWeb,
+                        path_html,
+                        path_js,
+                        path_css,
+                        doc_md,
+                        Keys_path,
+                    )
+
+    docker_file = """
+FROM python:3.9-slim
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copiar todos os arquivos do projeto
+COPY . /app
+
+
+    """
+
+
+
+    agent_ids = ['DockerFile']
+    agents_metadata = EgetMetadataAgent(agent_ids)
+
+    name = agents_metadata[f'{agent_ids[0]}']["name"]
+    model = agents_metadata[f'{agent_ids[0]}']["model"]
+    instruction = agents_metadata[f'{agent_ids[0]}']["instruction"]
+    try:
+      tools = agents_metadata[f'{agent_ids[0]}']["tools"]
+      Tools_Name_dict = Egetoolsv2(list(tools))
+    except:
+      pass
+
+    instruction_formatado = format_instruction(instruction, locals())
+
+    agent = Agent(
+        name=str(name),
+        instructions=f"""{RECOMMENDED_PROMPT_PREFIX}\n
+        {instruction_formatado}        
+        """,
+        model=str(model),
+        tools=Tools_Name_dict,
+        handoffs=[handoff_obj_CodeDockerComposeAgent],
+    )
+
+
+    handoff_obj = handoff(
+        agent=agent,
+        on_handoff=on_handoff,
+        input_type=FrontEndData,
+    )
+    return agent, handoff_obj
