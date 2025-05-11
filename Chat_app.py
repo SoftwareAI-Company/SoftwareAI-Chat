c1f7ccde999bf86fc5cf271cd667c175360be574
ualers <freitasalexandre810@gmail.com>
2025-05-11 10:26:16 -0300

    /api/AgentsWorkFlow/Saas/teams/BackEnd funcional

--- Chat_app.py
+++ Chat_app.py
@@ -2814,7 +2814,7 @@ def api_AgentsWorkFlow_Saas_teams_FrontEnd():
     }), 201
 
 @app.route('/api/AgentsWorkFlow/Saas/teams/BackEnd', methods=['POST'])
-@limiter.limit(lambda: dynamic_rate_limit(appcompany))
+@limiter.limit(lambda: dynamic_rate_limit(appcompany))  
 def api_AgentsWorkFlow_Saas_teams_BackEnd():
     data = request.get_json()
     session_id = data.get("session_id")
